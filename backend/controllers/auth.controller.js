4aeb3fcc0d76445a35a03869a59d7eccb6c7b5b0
Cameron <cameronlzy@gmail.com>
2025-09-10 01:01:25 +0800

    Add changes to backend CORS

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,8 +3,13 @@ import jwt from "jsonwebtoken"
 import User from "../models/user.model.js"
 import AppError from "../utils/appError.js"
 import { catchAsync } from "../utils/catchAsync.js"
-import { validateLogin, validateUser } from "../validators/user.validator.js"
+import {
+  validateEmailReq,
+  validateLogin,
+  validateUser,
+} from "../validators/user.validator.js"
 import { COOKIESETTING } from "../constants/cookieSetting.js"
+import { sendEmail } from "../utils/email.js"
 
 const signToken = (id) => {
   return jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -31,7 +36,7 @@ export const signup = catchAsync(async (req, res, next) => {
 })
 
 export const login = catchAsync(async (req, res, next) => {
-  // Check if email and password exist
+  // Check if email and password valid
   const { error: validationError, value } = validateLogin(req.body)
   if (validationError) {
     return next(new AppError(validationError.details[0].message, 400))
@@ -55,4 +60,42 @@ export const logout = catchAsync(async (req, res, next) => {
   // Set cookie with immediate expiry
   res.clearCookie("jwt", COOKIESETTING)
   res.status(200).json(success({ message: "Logged out successfully" }, 200))
+})
+
+export const emailVerification = catchAsync(async (req, res, next) => {
+  // Check if email valid
+  const { error: validationError, value } = validateEmailReq(req.body)
+  if (validationError) {
+    return next(new AppError(validationError.details[0].message, 400))
+  }
+
+  const user = await User.findOne({ email: value.email })
+  if (!user) {
+    return next(new AppError("User does not exist"), 404)
+  }
+
+  const tempAuthToken = signToken(user._id + "temporary_auth")
+  await sendEmail()
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 min)",
+      message,
+    })
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    })
+  } catch (err) {
+    user.passwordResetToken = undefined
+    user.passwordResetExpires = undefined
+    await user.save({ validateBeforeSave: false })
+
+    return next(
+      new AppError("There was an error sending the email. Try again later!"),
+      500
+    )
+  }
 })
